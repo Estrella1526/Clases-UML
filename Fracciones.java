9a3999fca2fea76a40a65819c62cb7338387ec1b
Estrella1526 <samestrella06@gmail.com>
2024-02-14 17:41:23 -0500

    index all comit

---
+++ Fracciones.java
@@ -0,0 +1,117 @@
+import java.util.*;
+
+/**
+ * 
+ */
+public class Fraccionario {
+
+    /**
+     * Default constructor
+     */
+    public Fraccionario() {
+    }
+
+    /**
+     * 
+     */
+    public int Numerador;
+
+    /**
+     * 
+     */
+    public int denominador;
+
+    /**
+     * 
+     */
+    public void Fraccionario() {
+        // TODO implement here
+    }
+
+    /**
+     * @param num 
+     * @param den
+     */
+    public void Fraccionario(int num, int den) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int obtenernumerador() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int obtenerDenominador() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param num 
+     * @return
+     */
+    public void establecerNumerador(int num) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param den 
+     * @return
+     */
+    public void establecerDenominador(int den) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void simplificar() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param other 
+     * @return
+     */
+    public Fraccionario sumar(Fraccionario other) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param other 
+     * @return
+     */
+    public Fraccionario restar(Fraccionario other) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param other 
+     * @return
+     */
+    public Fraccionario multiplicar(Fraccionario other) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param other 
+     * @return
+     */
+    public Fraccionario dividir(Fraccionario other) {
+        // TODO implement here
+        return null;
+    }
+
+}
